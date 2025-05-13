ff8bdbd667af8b020e109189f08a3412dbb24441
Lara <larathaylanne@gmail.com>
2025-05-13 18:49:41 -0300

    Aprendendo a usar o import usando a biblioteca MATH

---
+++ testante-imports.py
@@ -0,0 +1,7 @@
+#NA AULA DE HOJE, APRENDI A IMPORTAR E USAR MÓDULOS EM PYTHON, SEGUE ALGUNAS TESTEMQUE FIZ
+
+import math
+num = int(input("digite um número: \n"))
+raiz = math.sqrt(num)
+
+print("A raiz de {}, é igual a {}".format(num, raiz))
