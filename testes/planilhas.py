cab8e95f05db95fca798604bd2f0949eee6c6466
Lara Thaylanne <larathaylanne@gmail.com>
2025-08-16 12:40:12 -0300

    up

--- testes/planilhas.py
+++ testes/planilhas.py
@@ -1,19 +1,18 @@
 from openpyxl import load_workbook, Workbook
 
-#ler os dados da planilha
 
 planilha_country = load_workbook('Country AUTOMACAO.xlsx')
 pagina_principal = planilha_country['Cereais']
 
-for linha in pagina_principal.iter_rows(values_only=True):
-    print(linha)
-
-#AUTOMATIZAR PLANILHA 
-#inserir dados
 planilha_respondida = Workbook()
-cereais = planilha_country.active
+aba_nova = planilha_respondida.active
+aba_nova.tittle = 'Cereais'
+
+for linha in pagina_principal.iter_rows(values_only=True):
+    aba_nova.append(linha)
 
 with open ('precos-country.txt', 'r', encoding='utf-8') as arquivo:
     for linha in arquivo:
-        cereais.append(linha.split('    '))
-planilha_country.save('preenchendo_planilha.xlsx')
+        dados = linha.strip().split(',')
+        aba_nova.append(dados)
+planilha_respondida.save('preenchendo_planilha.xlsx')
